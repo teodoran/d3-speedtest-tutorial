f49df08f7468b95a5109c583fa3daa79d0e96505
teodoran <teodor@andeelstad.com>
2015-11-24 02:07:49 +0100

    Add very basic authentication to POST log calls

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 var express = require('express'),
-    dummyData = require('./dummy-data.json'),
+    bodyParser = require('body-parser'),
     app = express(),
-    port = process.env.PORT || 3000;
+
+    port = process.env.PORT || 3000,
+    secret = process.env.SECRET || 'NotSecret',
+    data = require('./dummy-data.json');
 
 app.use(express.static('client'));
 app.use(express.static('node_modules/d3/'));
 
+app.use(bodyParser.json());
+
+// curl -d '{"MyKey":"My Value"}' -H "Content-Type: application/json" -H "secret: NotSecret" -i http://127.0.0.1:3000/log
+app.post('/log', function(request, response){
+  if (request.headers['secret'] === secret) {
+    console.log(request.body);
+    response.status(200).end();
+  } else {
+    console.log('Unauthorized access attempt at /log');
+    response.status(401).end();
+  }
+});
+
 app.get('/history', function (req, res) {
-  res.json({dummyData});
+  res.json({data});
 });
 
 var server = app.listen(port, function () {
